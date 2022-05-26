b0d2f3e85a325f61448febb86bc3a51fe6962b5e
Arild Fines <arild.fines@broadpark.no>
2022-05-26 20:04:39 +0200

    Show activites color in the popup menu

---
+++ src/TimeyWimey/Infrastructure/ColorHelper.cs
@@ -0,0 +1,20 @@
+﻿using MudBlazor.Utilities;
+
+namespace TimeyWimey.Infrastructure
+{
+    public class ColorHelper
+    {
+        public static string CalculateForegroundTextColor(string? backgroundColor)
+        {
+            try
+            {
+                var color = backgroundColor != null ? new MudColor(backgroundColor) : new MudColor("#FF00FF");
+                return color.L > 0.45 ? "black" : "white";
+            }
+            catch (Exception)
+            {
+                return "black";
+            }
+        }
+    }
+}
