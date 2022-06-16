ce4a56d48fa433d0a7024ac3e5bf654b1fd7046e
Arild Fines <arild.fines@broadpark.no>
2022-06-16 23:12:11 +0200

    Get rid of the HttpClient DI registration as it is not required

--- src/TimeyWimey/Program.cs
+++ src/TimeyWimey/Program.cs
@@ -1,8 +1,5 @@
-using System.Runtime.InteropServices;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.JSInterop;
 using MudBlazor.Services;
 using TimeyWimey;
 using TimeyWimey.Data;
@@ -15,15 +12,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
 
-
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
 builder.Services.AddSingleton<Calendar>();
 builder.Services.AddSingleton<TimeLineCalculator>();
-
 builder.Services.AddSingleton<EventAggregator>();
 builder.Services.AddSingleton<MouseService>();
 builder.Services.AddSingleton<DataPersistence>();
